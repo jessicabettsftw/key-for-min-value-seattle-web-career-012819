76327e630f4e87cd97b5799c118fd7d289a4485a
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-16 01:49:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = 
+  min = name_hash.max_by{|k,v| v}
   print min
   name_hash.collect do |key, val|
     if val < min
