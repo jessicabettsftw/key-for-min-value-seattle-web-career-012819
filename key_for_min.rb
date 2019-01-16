b452dc0e57917e36b34a2607a4df74d6e5547fbf
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-16 01:57:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_person = name_hash.first
-  min = min_person.flatten(1)
+  min = 100000000000
   name_hash.collect do |key, val|
     if val < min
       min = val
