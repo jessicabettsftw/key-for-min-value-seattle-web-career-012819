a85498c1551227585e4ba3336bfd2c4ec725f7fa
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-16 01:56:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_person = name_hash.first
-  min = min_person.flatten(0)
+  min = min_person.flatten(1)
   name_hash.collect do |key, val|
     if val < min
       min = val
